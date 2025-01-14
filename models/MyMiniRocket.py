391da4c3c88e979f2a56051bab2dea33fb2d371e
davide <davide.serramazza@gmail.com>
2025-01-14 16:25:03 +0000

    MyMiniRocket: only MiniRocket available as transform (no Rocket)

--- models/MyMiniRocket.py
+++ models/MyMiniRocket.py
@@ -9,18 +9,16 @@ import torch.nn as nn
 from .tsai.MINIROCKET_Pytorch import MiniRocketFeatures, get_minirocket_features
 
 from .aaltd2024.code.ridge import RidgeClassifier
-from .aaltd2024.code.utils import Dataset
 
-# TODO should I change module and file name??
 class MyMiniRocket(nn.Module):
 
-    def __init__(self, n_channels,seq_len,n_classes,normalise=True,chunk_size=32, device="cpu", verbose=False):
+    def __init__(self, n_channels,seq_len,n_classes,chunk_size=32, device="cpu", verbose=False):
+        # TODO implement the (non) verbose mode
         super(MyMiniRocket, self).__init__()
 
         self.n_channels = n_channels
         self.seq_len = seq_len
         self.n_classes = n_classes
-        self.normalise = normalise
         self.chunk_size = chunk_size
         self.device = device
         self.f_mean =None ; self.f_std = None
@@ -31,20 +29,6 @@ class MyMiniRocket(nn.Module):
         self.classifier = RidgeClassifier(self.transformer_model,device=device)
         self.to(device)
         self.trained = False
-
-    """
-    def _predict (self,X, batch_size=32):
-        outs = []
-        n = X.shape[0]
-
-        with torch.no_grad():
-            for i in range(0, n,  batch_size):
-                curr_X = X[i:min(i+batch_size,n)]
-                curr_y = self.classifier._predict(curr_X)
-                outs.append(curr_y.cpu().detach().numpy())
-
-        return np.concatenate(outs,axis=0) # TODO hardcoded! torch.tensor(outs)
-    """
 
     def train(self,data_train):
 
