95186d0e7b9f378488b5b1b0e72ad13a8c9e4a95
davide <davide.serramazza@gmail.com>
2025-01-28 10:42:22 +0000

    restructured trainers s.t. explain ratio is optional

--- utils/trainers.py
+++ utils/trainers.py
@@ -1,3 +1,5 @@
+import torch
+
 from models.aaltd2024.code.hydra_gpu import HydraMultivariateGPU
 from models.aaltd2024.code.ridge import RidgeClassifier
 from models.aaltd2024.code.quant import QuantClassifier as QuantClassifier_aaltd
@@ -10,15 +12,19 @@ from models.convTran import build_train_ConvTran
 
 def trainScore_hydra_gpu( dataset , device, batch_size ):
 
+    explain_set = 'explain_set' in dataset.keys()
+
     # get different dataset parts
     X_train, y_train =      dataset['train_set']['X'] , dataset['train_set']['y']
-    X_explain, y_explain =  dataset['explain_set']['X'] , dataset['explain_set']['y']
     X_test, y_test =        dataset['test_set']['X'] , dataset['test_set']['y']
 
     # TODO do I need dataset only for score???
     data_train = Dataset(X_train, y_train, batch_size=batch_size, shuffle=True)
-    data_explain = Dataset(X_explain, y_explain, batch_size=batch_size, shuffle=False)
     data_test = Dataset(X_test, y_test, batch_size=batch_size, shuffle=False)
+
+    if explain_set:
+        X_explain, y_explain =  dataset['explain_set']['X'] , dataset['explain_set']['y']
+        data_explain = Dataset(X_explain, y_explain, batch_size=batch_size, shuffle=False)
 
     # extract TS info
     _ , n_channels, length = X_train.shape
@@ -28,7 +34,7 @@ def trainScore_hydra_gpu( dataset , device, batch_size ):
     model = RidgeClassifier(transform=transform, device=device)
     model.fit(data_train, num_classes=n_classes)
 
-    error_explain_set = model.score(data_explain)
+    error_explain_set = model.score(data_explain) if explain_set else torch.tensor([2])
     error_test_set  =   model.score(data_test)
 
     return  (1 - error_explain_set.cpu().numpy().item()) , (1 - error_test_set.cpu().numpy().item()), model
@@ -37,14 +43,18 @@ def trainScore_hydra_gpu( dataset , device, batch_size ):
 
 def train_Minirocket_ridge_GPU(  dataset , device, batch_size ):
 
+    explain_set = 'explain_set' in dataset.keys()
+
     # get different dataset parts
     X_train, y_train =      dataset['train_set']['X'] , dataset['train_set']['y']
-    X_explain, y_explain =  dataset['explain_set']['X'] , dataset['explain_set']['y']
     X_test, y_test =        dataset['test_set']['X'] , dataset['test_set']['y']
 
     data_train = Dataset(X_train, y_train, batch_size=batch_size, shuffle=True)
-    data_explain = Dataset(X_explain, y_explain, batch_size=batch_size, shuffle=False)
     data_test = Dataset(X_test, y_test, batch_size=batch_size, shuffle=False)
+
+    if explain_set:
+        X_explain, y_explain =  dataset['explain_set']['X'] , dataset['explain_set']['y']
+        data_explain = Dataset(X_explain, y_explain, batch_size=batch_size, shuffle=False)
 
     # extract TS info
     n_samples , n_channels , seq_len = X_train.shape
@@ -53,10 +63,10 @@ def train_Minirocket_ridge_GPU(  dataset , device, batch_size ):
     model = MyMiniRocket(n_channels=n_channels,seq_len=seq_len,n_classes=n_classes, device=device)
     model.train(data_train)
 
-    acc_explain = model.score(data_explain)
-    acc_test = model.score(data_test)
+    acc_explain_set = model.score(data_explain) if explain_set else np.array([-1])
+    acc_test_set = model.score(data_test)
 
-    return acc_explain.item(), acc_test.item(), model
+    return acc_explain_set.item(), acc_test_set.item(), model
 
 
 def train_ConvTran( dataset , device, batch_size, verbose=False ):
@@ -67,7 +77,7 @@ def train_ConvTran( dataset , device, batch_size, verbose=False ):
                                                  save_path=None,verbose=verbose)
     convTran.eval()
 
-    accuracy_explainSet =   convTran.score(explain_loader)
+    accuracy_explainSet =   convTran.score(explain_loader) if explain_loader!=None else np.array([-1])
     accuracy_testSet =      convTran.score(test_loader)
 
     return accuracy_explainSet.item(), accuracy_testSet.item(), convTran
