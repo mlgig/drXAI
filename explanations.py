2c8187ba8d141a71a278de1c32b9d345ea7cb534
davide <davide.serramazza@gmail.com>
2025-01-30 17:19:20 +0000

    tmp main

--- explanations.py
+++ explanations.py
@@ -3,7 +3,7 @@ from tsCaptum.explainers import Feature_Ablation, Shapley_Value_Sampling
 from windowshap import MyWindowSHAP
 import timeit
 
-def tsCaptum_explainations(current_experiment ,model, X, y, batch_size):
+def tsCaptum_explainations(current_experiment ,model, X, y, batch_size,background):
 	for (explainer, explainer_name) in [
 		(Feature_Ablation, 'Feature_Ablation'), (Shapley_Value_Sampling, 'Shapley_Value_Sampling')
 	]:
@@ -11,20 +11,16 @@ def tsCaptum_explainations(current_experiment ,model, X, y, batch_size):
 		current_experiment[explainer_name] = {}
 		attribution = explainer(model)
 		for n_segments in [1, 5, 10]:
-			# TODO think about more baseline!
 			start_ex = timeit.default_timer()
-
-			saliency_maps = attribution.explain(samples=X, labels=y, n_segments=n_segments,normalise=True,baseline=0,
-											   batch_size=batch_size)
+			saliency_maps = attribution.explain(samples=X, labels=y, n_segments=n_segments,normalise=True,
+									baseline=background,batch_size=batch_size)
 			current_experiment[explainer_name][n_segments] = {
 				'result': saliency_maps,
 				'explaining_time': (timeit.default_timer() - start_ex)
 			}
 
-def windowSHAP_explanations(current_experiment,model, X_explain, to_terminate):
+def windowSHAP_explanations(current_experiment,model, X_explain, to_terminate,background_data):
 	# so far hard coded 0 baseline # TODO get more baselines
-	background_data = X_explain[0:1]*0
-
 	n_instances, n_channels ,n_time_points = X_explain.shape
 	w_len =  int(n_time_points/5)	; stride = max(int(n_time_points/10),1)
 	print(w_len,stride)
@@ -39,8 +35,8 @@ def windowSHAP_explanations(current_experiment,model, X_explain, to_terminate):
 	saliency_maps = []
 	# explain instance by instance
 	for i in range(X_explain.shape[0]):
-		current_saliency_map = MyWindowSHAP(model.predict_proba, test_data = X_explain[i:i+1], background_data = background_data,
-												window_len = w_len, stride = stride, method = 'sliding').shap_values()
+		current_saliency_map = MyWindowSHAP(model.predict_proba, test_data = X_explain[i:i+1],
+				background_data = background_data,window_len = w_len, stride = stride, method = 'sliding').shap_values()
 		saliency_maps.append(current_saliency_map)
 		# is termination flag has been set by the main thread break the loo[
 		if to_terminate.is_set():
--- train_explain.py
+++ train_explain.py
@@ -4,6 +4,7 @@ from threading import Thread, Event
 from utils.trainers import train_ConvTran, train_Minirocket_ridge_GPU, trainScore_hydra_gpu
 from utils.data_utils import *
 from explanations import tsCaptum_explainations, windowSHAP_explanations
+from utils.backgrounds import class_prototypes_avg, smote_avg, equal_distributed_proba
 
 # TODO same datatype or numpy array for predict and also for score?
 # TODO avoid double conversion back and fort from ndarray to torch.Tensor for aaltd ridge?
@@ -20,16 +21,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 results = {}
 
 ####################### load dataset ##################################
-bast_path = "/media/davide/DATA/datasets/Multivariate2018_ts/"
 
-for dir in ["Multivariate_ts", "others_new/", "others_MTSCcom"]:
-	for current_dataset in sorted(os.listdir(os.path.join(bast_path,dir))): # datasets:
+for dir in [ "others_new"]:#,,  "Multivariate_ts", "others_MTSCcom"]:
+	for current_dataset in ['MP50']: #sorted(os.listdir(os.path.join(bast_path,dir))): # datasets:
 
 		print(current_dataset)
 		# load data	# TODO same code for both!
 		dataset_dir =  os.path.join(bast_path,dir,current_dataset)
-		data = load_datasets(dataset_dir, current_dataset, explain_set_ratio=0.2)
-
+		data = load_datasets(dataset_dir, current_dataset)
 
 		# create an entry in result's data structure. Save 'symbolic label -> numeric label' map
 		results[current_dataset] = {'labels_map' : data['labels_map']}
@@ -37,7 +36,7 @@ for dir in ["Multivariate_ts", "others_new/", "others_MTSCcom"]:
 		#################################### train model #################################################
 
 		for model_name,  trainer, batch_size in [('ConvTran', train_ConvTran,32),
-					('miniRocket', train_Minirocket_ridge_GPU,64) ,('hydra', trainScore_hydra_gpu,128)
+			('miniRocket', train_Minirocket_ridge_GPU,64) ,('hydra', trainScore_hydra_gpu,128)
 			] :	#('QUANT', train_QUANT_aaltd2024,128)
 
 			# check if current combination of dataset and model is a special case
@@ -52,32 +51,40 @@ for dir in ["Multivariate_ts", "others_new/", "others_MTSCcom"]:
 
 			# train and save model along with additional info about the training
 			save_model_path = os.path.join("saved_models", "_".join((current_dataset,model_name))+".pth")
-			accuracy_explain , accuracy_test, model = trainer( dataset = data ,device = device, batch_size=batch_size )
+			accuracy_test, X_train_pred, model = trainer( dataset = data ,device = device, batch_size=batch_size )
+			print(model_name , "'s train completed, accuracy was",accuracy_test)
+
+			torch.save(model,save_model_path)
 
 			results[current_dataset][model_name] = {
-				"accuracy_explain"	:accuracy_explain,
 				"accuracy_test"		:accuracy_test,
 				'training_time'		:(timeit.default_timer() -start_tr)
 			}
 
-			torch.save(model,save_model_path)
-			print(model_name , 'train completed: Accuracy on explain set',accuracy_explain, 'and test set' , accuracy_test)
-
 			################################ explain ###########################################
 			# get explaining set's features and labels
-			X_to_explain , labels = data['explain_set']['X'] , data['explain_set']['y']
+			X_to_explain , labels = data['train_set']['X'] , data['train_set']['y']
 
-			tsCaptum_explainations(current_experiment=results[current_dataset][model_name],model=model,
-								   X=X_to_explain, y=labels, batch_size=batch_size)
+			backgrounds = [
+				X_to_explain[0:1]*0,									#zeros background
+				smote_avg(X_to_explain,labels),
+				class_prototypes_avg(X_to_explain,labels),
+				equal_distributed_proba(X_to_explain,X_train_pred),
+			]
 
-			# use trheading for windowSHAP
-			to_terminate = Event()
-			p = Thread(target = windowSHAP_explanations, args = [
-				results[current_dataset][model_name], model,X_to_explain,to_terminate
-			])
 
-			# set a termination flag after have joined current thread for 24 hours (60*60*24 seconds)
-			p.start()	; 	p.join(timeout = 60*60*24)	;	to_terminate.set()	; p.join()
+			for background in backgrounds:
+
+				tsCaptum_explainations(current_experiment=results[current_dataset][model_name],model=model,
+									   X=X_to_explain, y=labels, batch_size=batch_size,background=background)
+
+				# use trheading for windowSHAP
+				to_terminate = Event()
+				p = Thread(target = windowSHAP_explanations, args = [
+					results[current_dataset][model_name], model,X_to_explain,to_terminate, background
+				])
+				# set a termination flag after have joined current thread for 24 hours (60*60*24 seconds)
+				p.start()	; 	p.join(timeout = 60*60*24)	;	to_terminate.set()	; p.join()
 
 			# dump result data structure on disk
-			np.savez_compressed(os.path.join("results","experiment_results.npz"), results=results)
+			np.savez_compressed(os.path.join("results","MP8.npz"), results=results)
