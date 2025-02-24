4e733f081d1c49d1864a4e66db1af96d9ff303ad
davide <davide.serramazza@gmail.com>
2025-02-24 11:51:52 +0000

    compute_accuracies.py refactore for NON initial train

--- compute_accuracies.py
+++ compute_accuracies.py
@@ -1,25 +1,13 @@
-import numpy as np
-import pandas as pd
-from copy import deepcopy
 import argparse
+from copy import deepcopy
 from pprint import pprint
-import timeit
 
-from utils.trainers import train_ConvTran, train_Minirocket_ridge_GPU, trainScore_hydra_gpu
 from utils.data_utils import *
+from utils.get_accuracy import get_accuracies
 
 # get device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 base_path =  #TODO argument
 
-# TODO move into separate shared file
-special_cases = {
-	('EigenWorms' , 'ConvTran') : 'skip',
-	('PenDigits', 'miniRocket') : 'skip',
-	('MotorImagery', 'ConvTran') : 16 ,
-	('Tiselac', 	'ConvTran') :  4096,
-	('PenDigits', 	'ConvTran') :  1024
-}
 
 def get_AI_selections(saliency_map_dict, result_dict, info):
 
@@ -29,11 +17,26 @@ def get_AI_selections(saliency_map_dict, result_dict, info):
 		elif k=='selected_channels':
 			model, explainer = info.split("_")[1] , "_".join( info.split("_")[2:] )
 			result_dict[model][explainer] = saliency_map_dict[k]
-		#result_dict[model][explainer+"_cd_selection"] = cd_selection
 		elif type(saliency_map_dict[k])==dict :
 			get_AI_selections(saliency_map_dict[k],result_dict, info+"_"+str(k))
 
 	return result_dict
+
+
+def add_mostAccurate(all_selections,initial_accuracies):
+	# get most accurate
+	most_accurate_model = max(initial_accuracies, key=lambda model: initial_accuracies[model]['initial_accuracy']['best'])
+	AI_selections = ['smote_background_Feature_Ablation', 'smote_background_Shapley_Value_Sampling',
+					 'prototypes_background_Feature_Ablation', 'prototypes_background_Shapley_Value_Sampling']
+	best_model_AI_selections = [(selection,all_selections[most_accurate_model][selection]) for selection in AI_selections]
+
+	# add this selection to other models
+	other_models = set(initial_accuracies.keys()).difference(set([most_accurate_model]))
+	for model in other_models:
+		for (name,selection) in best_model_AI_selections:
+			all_selections[model]["most_accurate_model_"+name] =selection
+
+	return all_selections
 
 
 def get_elbow_selections(current_data,elbows):
@@ -42,71 +45,6 @@ def get_elbow_selections(current_data,elbows):
 		'elbow_sum' : elbows[current_data]['Sum']
 	}
 
-def get_accuracies(original_data,save_models_path, channel_selections, initial_accuracies=None):
-
-	# get info
-	current_dataset = original_data['name']
-	accuracies = {	'accuracies'	: {}	}
-	current_dataset_dict = accuracies['accuracies']
-
-
-
-	for clf_name, trainer, batch_size in [
-		('hydra', trainScore_hydra_gpu, 128),
-		('ConvTran', train_ConvTran, 32),
-		('miniRocket', train_Minirocket_ridge_GPU, 64),
-
-		]:
-
-		current_dataset_dict[clf_name] = {} if initial_accuracies is None else {
-			'initial_accuracy' : initial_accuracies[current_dataset][clf_name]['all_channels']
-		}
-
-		for exp_name, selection in channel_selections[clf_name].items():
-			# accuracies vector
-			current_dataset_accs = np.zeros(shape=(5,))
-
-			# get current selected channels
-			data  = deepcopy(original_data)
-			data['train_set']['X'] = data['train_set']['X'][:,selection,:]
-			data['test_set']['X'] = data['test_set']['X'][:,selection,:]
-
-			#print("current selection is",selection, data['train_set']['X'].shape, data['train_set']['X'].shape)
-			# check if current combination of dataset and model is a special case
-			if (current_dataset,clf_name) in special_cases:
-				if special_cases[(current_dataset,clf_name)]=='skip':
-					continue
-				else:
-					batch_size = special_cases[(current_dataset,clf_name)]
-
-			saved_models_path = os.path.join(save_models_path, "_".join((current_dataset,clf_name,exp_name))+".pth")
-
-			# train 5 times
-			for i in range(5):
-				star_time = timeit.default_timer()
-				current_accuracy, _ , model = trainer(dataset=data, device=device, batch_size=batch_size)
-				total_time = timeit.default_timer() - star_time
-				current_dataset_accs[i] = current_accuracy
-
-				# TODO optional hyperparam??
-				# save best model
-				if max(current_dataset_accs)==current_accuracy:
-					torch.save(model,saved_models_path)
-					training_time = total_time
-
-
-			# extrac mean, std deviation and best accuracy
-			current_dataset_dict[clf_name][exp_name]	 = 	{
-				'training_time' : training_time,
-				'mean' : np.mean(current_dataset_accs).item(),
-				'std' : np.std(current_dataset_accs).item() ,
-				'best' :  np.max(current_dataset_accs).item(),
-				'channels' : selection
-			}
-
-			#("mean and std accuracy was", 	(np.mean(current_acc) ,np.std(current_acc)), "\n\n")
-
-	return current_dataset_dict
 
 def main(args):
 
@@ -142,8 +80,11 @@ def main(args):
 				# if not initial accuracy mode get elbow and AI selection
 				elbow_selections = get_elbow_selections(current_dataset,all_elbow_selections)
 				all_selections = get_AI_selections(saliency_maps[current_dataset],{
-					'ConvTran' : elbow_selections, 'miniRocket': elbow_selections, 'hydra' : elbow_selections
+					'ConvTran' : deepcopy(elbow_selections),
+					'miniRocket': deepcopy(elbow_selections),
+					'hydra' : deepcopy(elbow_selections)
 				},"")
+				all_selections = add_mostAccurate(all_selections,initial_accuracies[current_dataset])
 
 			else:
 				# otherwise compute the accuracy using all available channel
@@ -160,8 +101,6 @@ def main(args):
 			all_accuracies[current_dataset] = current_accuracies
 
 			np.save( result_path,all_accuracies)
-
-
 
 
 if __name__ == '__main__':
