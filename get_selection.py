7326c92f8425c3bc3b5e10b662b540563dcf5535
davide <davide.serramazza@gmail.com>
2025-03-06 13:59:01 +0000

    get_selection.py completely refactored

--- get_selection.py
+++ get_selection.py
@@ -1,102 +1,127 @@
 import timeit
 from threading import Thread, Event
+import argparse
 
-from utils.trainers import train_ConvTran, train_Minirocket_ridge_GPU, trainScore_hydra_gpu
+import numpy as np
+
+from utils.trainers import trainer_list
 from utils.data_utils import *
-from explanations import tsCaptum_explainations, windowSHAP_explanations, tsCaptum_selection
+from utils.batchSizes_exceptions import batch_sizes as batches_dict, ToSkip_batchSize
+from explanations import windowSHAP_explanations, tsCaptum_selection
 from utils.backgrounds import class_prototypes_avg, smote_avg, equal_distributed_proba
 
 # TODO same datatype or numpy array for predict and also for score?
 # TODO avoid double conversion back and fort from ndarray to torch.Tensor for aaltd ridge?
 # TODO do I need tensorboard for ConvTran?
 
-special_cases = {
-	('EigenWorms' , 'ConvTran') : 'skip',
-	('PenDigits', 'miniRocket') : 'skip',
-	('MotorImagery', 'ConvTran') : 16
-}
+def main(args):
 
-# get device and instantiate result data structure
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-results = {}
+	#get arguments
+	base_path = args.dataset_dir
+	saved_models_dir = args.saved_models_path
+	model_result_path = args.model_result_path
+	results_path = args.explainer_results_path
+	random_seed = args.random_seed
 
-####################### load dataset ##################################
+	# get device, set random seed and instantiate result data structure
+	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	torch.manual_seed(random_seed)
+	results = {}
+	models_info = {}
 
+	# load dataset
+	# TODO hard coded
+	for dir_n in [ "Multivariate_ts", "others_new", "others_MTSCcom"]:
+		for current_dataset in  sorted(os.listdir(os.path.join(base_path,dir_n))): # datasets:
 
-base_path = "/media/davide/DATA/datasets/Multivariate2018_ts/"
+			dataset_dir =  os.path.join(base_path,dir_n,current_dataset)
+			data = load_datasets(dataset_dir, current_dataset)
 
-for dir_n in [ "Multivariate_ts", "others_new", "others_MTSCcom"]:
-	for current_dataset in sorted(os.listdir(os.path.join(base_path,dir_n))): # datasets:
+			print("\n\n current loaded dataset is", current_dataset)
 
-		dataset_dir =  os.path.join(base_path,dir_n,current_dataset)
-		data = load_datasets(dataset_dir, current_dataset)
+			# TODO hard coded
+			if data['train_set']['X'].shape[1] < 8:
+				continue
 
-		# create an entry in result's data structure. Save 'symbolic label -> numeric label' map
-		results[current_dataset] = {'labels_map' : data['labels_map']}
+			# create an entry in result's data structure. Save 'symbolic label -> numeric label' map
+			results[current_dataset] = {'labels_map' : data['labels_map']}
+			models_info[current_dataset] = {}
 
-		#################################### train model #################################################
+			############################# train ####################################
+			# train current classifier
+			for model_name, trainer in trainer_list:
 
-		# set which models to use and initialize result data structure
-		models_batchSizes = [
-			('hydra',128),
-			('ConvTran',32),
-			('miniRocket',64) ,
-		]
-
-		for name,_ in models_batchSizes:
-			results[current_dataset].setdefault(name,{})
-
-		for model_name, batch_size in models_batchSizes:
-
-			# check if current combination of dataset and model is a special case
-			if (current_dataset,model_name) in special_cases:
-				if special_cases[(current_dataset,model_name)]=='skip':
+				# check whether to skip or not current classifier/dataset combination
+				to_skip, batch_size = ToSkip_batchSize(current_dataset, model_name)
+				if to_skip:
 					continue
 				else:
-					bast_path =  special_cases[(current_dataset,model_name)]
+					# if not to skip record accuracy and save model to disk
+					start_time = timeit.default_timer()
+					current_accuracy , model = trainer(dataset=data, device=device, batch_size=batch_size)
+					training_time = timeit.default_timer() - start_time
 
-			# otherwise load current model
-			save_model_path = os.path.join("saved_models", "_".join((current_dataset,model_name))+".pth")
-			model = torch.load(save_model_path)
-
-			################################ explain ###########################################
-			# get explaining set's features and labels
-			X_to_explain , labels = data['train_set']['X'] , data['train_set']['y']
-
-			# define backgrounds to be tested
-			backgrounds = [
-				#('zeros_background', X_to_explain[0:1]*0	),									#zeros background
-				('smote_background',smote_avg(X_to_explain,labels)	),
-				('prototypes_background' ,class_prototypes_avg(X_to_explain,labels)	),
-				#('equal_distributed_background',equal_distributed_proba(X_to_explain,X_train_pred)	),
-				#('42s_background' ,np.ones((	1, X_to_explain.shape[1], X_to_explain.shape[2]))*42	)	# meaningless background
-			]
-
-			for name, _ in backgrounds:
-				results[current_dataset][model_name].setdefault(name, {})
-
-			for b_name,background in backgrounds:
-
-				results[current_dataset][model_name][b_name] = {}
-
-				for alg in ['Feature_Ablation' ,'Shapley_Value_Sampling']:
-					# for each background initialise result dict; then explain
-					chs_selected, attribution = tsCaptum_selection(model=model,X=X_to_explain,y=labels,
-						batch_size=batch_size,background=background,explainer_name=alg, return_saliency=True)
-					results[current_dataset][model_name][b_name][alg] = {
-						'selected_channels' : chs_selected,
-						'saliency_map' : attribution
+					file_name = "_".join((current_dataset,model_name,"allChannel"))+".pth"
+					torch.save(model, os.path.join(saved_models_dir,file_name))
+					models_info[current_dataset][model_name] = {
+						"training_time" : training_time,
+						'accuracy' : current_accuracy,
 					}
 
-					# use threading for windowSHAP
-				#to_terminate = Event()
-				#p = Thread(target = windowSHAP_explanations, args = [
-				#	current_experiment, model,X_to_explain,to_terminate, background
-				#])
-				# set a termination flag after have joined current thread for 24 hours (60*60*24 seconds)
-				#p.start()	; 	p.join(timeout = 60*60*24)	;	to_terminate.set()	; p.join()
 
-				print('\t', model_name, b_name, 'combination computed')
-				# dump result data structure on disk
-				np.savez_compressed(os.path.join("results","saliency_maps.npz"), results=results)
+				results[current_dataset][model_name] = {}
 
+				################################ explain ###########################################
+				# get explaining set's features and labels
+				X_to_explain , labels = data['train_set']['X'] , data['train_set']['y']
+
+				# define backgrounds to be tested
+				backgrounds = [
+					('zerosBackground', X_to_explain[0:1]*0	),									#zeros background
+					('smoteBackground',smote_avg(X_to_explain,labels)	),
+					('prototypesBackground' ,class_prototypes_avg(X_to_explain,labels)	),
+				]
+
+				for b_name,background in backgrounds:
+
+					# for each background initialise result dict, then explain
+					results[current_dataset][model_name][b_name] = {}
+
+					for alg in ['Feature_Ablation' ,'Shapley_Value_Sampling']:
+						ch_selections, attribution, exp_time = tsCaptum_selection(model=model,X=X_to_explain,y=labels,
+							batch_size=batch_size,background=background,explainer_name=alg, return_saliency=True)
+
+						# save saliency map, selections,
+						results[current_dataset][model_name][b_name][alg] = {
+							'selected_channels_absolute' : ch_selections[0],
+							'selected_channels_PosNeg' : ch_selections[1],
+							'saliency_map' : attribution,
+							'explaining_time' : exp_time
+						}
+
+						# use threading for windowSHAP
+					#to_terminate = Event()
+					#p = Thread(target = windowSHAP_explanations, args = [
+					#	current_experiment, model,X_to_explain,to_terminate, background
+					#])
+					# set a termination flag after have joined current thread for 24 hours (60*60*24 seconds)
+					#p.start()	; 	p.join(timeout = 60*60*24)	;	to_terminate.set()	; p.join()
+
+					print('\t', model_name, b_name, 'combination computed')
+
+					# dump result data structure on disk
+					np.savez_compressed(results_path, results=results)
+					np.save( model_result_path, models_info)
+
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument("dataset_dir", type=str, help="folder where datasets are stored")
+	parser.add_argument("saved_models_path", type=str, help="folder where to saved models")
+	parser.add_argument("model_result_path", type=str, help="path where to save info about model")
+	parser.add_argument("explainer_results_path", type=str, help="path where to save explanations results"
+																 "i.e. saliency map and selected channels/time points")
+	parser.add_argument("random_seed", type=int, help="random seed to be used for reproducibility")
+	args = parser.parse_args()
+	main(args)
